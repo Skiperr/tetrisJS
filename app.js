006538856af01892165bbcdc9003081222b12ef7
Marek Danowski <danowski.marek@gmail.com>
2020-07-14 00:12:45 +0200

    moveleft

--- app.js
+++ app.js
@@ -44,12 +44,48 @@ const iTetromino = [
 const theTetrominos = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
 
   let currentPosition = 4
-  let current = theTetrominos[0][0]
+  let currentRotation = 0
+  let random = Math.floor(Math.random()*theTetrominos.length)
+
+  let current = theTetrominos[random][currentRotation]
 
 function draw() {
   current.forEach(index => {
     squares[currentPosition + index].classList.add('tetromino')
   })
 }
+
+function undraw() {
+  current.forEach(index =>{
+    squares[currentPosition + index].classList.remove('tetromino')
+  })
+}
+timerId = setInterval(moveDown,100)
+function moveDown(){
+  undraw()
+  currentPosition += width;
+  draw()
+  freeze()
+}
+
+function freeze(){
+  if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+    current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+    random = Math.floor(Math.random() *  theTetrominos.length)
+    current = theTetrominos[random][currentRotation]
+    currentPosition = 4;
+    draw()
+
+  }
+}
+
+function moveLeft() {
+undraw()
+const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+  if(!isAtLeftEdge) currentPosition -=1
+
+
+}
+
 
 })
